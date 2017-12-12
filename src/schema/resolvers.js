6e6bc75ee770aa7913766141a4999433eb9a161d
oliverlai55 <oliverlai55@gmail.com>
2017-12-12 01:43:01 -0500

    Got mongodb to save data via mutation

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -3,19 +3,28 @@ const ObjectId = mongoose.SchemaType.ObjectId;
 
 module.exports = {
   Query: {
-    allLinks: () => async (root, data, {mongo: {Links}}) => {
-      return await Links.find({}).toArray();
-    },
+    allLinks: async (parent, args, { Links }) => await Links.find(),
   },
-
   Mutation: {
-    createLink: async (root, data, {mongo: {Links}}) => {
-      const response = await Links.insert(data);
-      return Object.assign({id: response.insertedIds[0]}, data);
-    },
-  },
+    createLink: async (parent, args, { Links }) => await new Links(args).save()
+  }
+}
 
-  Link: {
-    id: root => root._id || root.id,
-  },
-};
+// module.exports = {
+//   Query: {
+//     allLinks: () => async (root, data, {mongo: {Links}}) => {
+//       return await Links.find({}).toArray();
+//     },
+//   },
+//
+//   Mutation: {
+//     createLink: async (root, data, {mongo: {Links}}) => {
+//       const response = await Links.insert(data);
+//       return Object.assign({id: response.insertedIds[0]}, data);
+//     },
+//   },
+//
+//   Link: {
+//     id: root => root._id || root.id,
+//   },
+// };
